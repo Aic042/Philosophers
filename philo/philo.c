d71d94dc7f4e6f6496a30868a28ae616f94aba5e
root <root@DESKTOP-7CAO994.>
2025-05-17 11:18:57 +0200

    2025-05-17 11:18:57

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 19:37:06 by root              #+#    #+#             */
-/*   Updated: 2025/05/14 20:16:21 by root             ###   ########.fr       */
+/*   Updated: 2025/05/17 11:18:55 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 int main(int argc, char **argv)
 {
-	(void)argv;
-	if (argc != 5 && argc != 6)
-		return (write(1, "Bad Argc", 9));
+	arg_checker(argc, argv);
 	return (write(1, "Done\n", 5), 0);
 }
 
 void arg_checker(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
-		return (write(1, "Bad Argc", 9));
-	return (write(1, "Done\n", 5), 0);
+		return (write(1, "Bad Argc\n", 10));
+	return (ft_printf("Args are all good"), 0);
 }
 
 //for later, should check if it's better to have an int
