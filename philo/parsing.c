41f8a4b181d08dd83db073e7c0d95dcb2a23be52
root <root@DESKTOP-7CAO994.localdomain>
2025-07-24 10:33:00 +0200

    2025-07-24 10:33:00

--- philo/parsing.c
+++ philo/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aingunza <aingunza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 12:49:03 by aingunza          #+#    #+#             */
-/*   Updated: 2025/07/19 14:25:13 by aingunza         ###   ########.fr       */
+/*   Updated: 2025/07/23 23:31:25 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	arg_checker(int argc, char **argv)
 	if (!check_pos_int(ft_atol(argv[1])) || !check_pos_int(ft_atol(argv[2]))
 		|| !check_pos_int(ft_atol(argv[3])) || !check_pos_int(ft_atol(argv[4])))
 		return (0);
-	if (ft_atol(argv[1]) == 0)
+	if (ft_atol(argv[1]) <= 0)
 		return (printf("No philosophers\n"), 0);
 	if (ft_atol(argv[3]) == 0 || ft_atol(argv[4]) == 0)
 		return (printf("Num is bad\n"), 0);
